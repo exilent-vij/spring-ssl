d31cc0b68734006e59ef01f896fb9d2558c0fb10
paras <vi@exilent.systems>
2019-06-22 16:01:19 +0530

    Intial Coomit

---
+++ src/test/java/exilent/systems/ffa/FfaApplicationTests.java
@@ -0,0 +1,16 @@
+package exilent.systems.ffa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FfaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
